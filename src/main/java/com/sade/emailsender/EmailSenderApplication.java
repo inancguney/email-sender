750d67ce83bfff9e6a5d614d70f6be65ab4e7cd1
tamercankacak <tamercankacak@gmail.com>
2022-07-19 11:03:24 +0300

    feat: EmailTemplates

--- src/main/java/com/sade/emailsender/EmailSenderApplication.java
+++ src/main/java/com/sade/emailsender/EmailSenderApplication.java
@@ -1,9 +1,9 @@
 package com.sade.emailsender;
 
 
+import com.google.gson.Gson;
 import com.sade.emailsender.dto.EmailTemplate;
 import com.sade.emailsender.service.FileReader;
-import com.sade.emailsender.service.JSONObject;
 import com.sade.emailsender.service.Sender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -26,21 +29,11 @@ public class EmailSenderApplication {
     }
 
     @EventListener(ApplicationReadyEvent.class)
+    public void sendmail() {
+        String jsonString = fileReader.readFile();
+        Gson g = new Gson();
+        ArrayList<EmailTemplate> emailTemplates = g.fromJson(jsonString, ArrayList.class);
 
-
-        public void sendmail() {
-        fileReader.readFile();
-        String new_data = fileReader.toString();
-        JSONObject obj = new JSONObject(new_data);
-        String n = obj.getString("to");
-        String m = obj.getString("body");
-        String k = obj.getString("subject");
-
-
-        EmailTemplate emailTemplate = new EmailTemplate("n", "k", "m");
-
-        SimpleMailMessage simpleMailMessage = sender.setSimpleMailMessage(emailTemplate);
-        sender.sendEmail(simpleMailMessage);
 
     }
 }
--- src/main/java/com/sade/emailsender/service/FileReader.java
+++ src/main/java/com/sade/emailsender/service/FileReader.java
@@ -5,17 +5,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
+
 @Service
 @Component
 public class FileReader {
-    public void readFile() {
-        try{
+    public String readFile() {
+        try {
 
             FileInputStream fis = new FileInputStream("src/main/resources/mail_detail.json");
             String data = IOUtils.toString(fis, "UTF-8");
-
-        } catch(Exception e){
-
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Illegal Argument");
         }
     }
 }
